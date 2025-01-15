e070c78d0f986ed107564e0c726134ae4ce7dec1
jitender4674 <gauravbudhwar9@gmail.com>
2025-01-15 17:03:03 +0530

    added second user account

--- script.js
+++ script.js
@@ -19,7 +19,26 @@ const account1 = {
     locale: 'en-IN',
 };
 
-const accounts = [account1];
+const account2 = {
+    owner: 'Gaurav Budhwar',
+    pin: 2222,
+    movements: [20000, -2000, 800, -400, -100, -20, -20, -100],
+    interestRate: 1.2,
+    movementsDates: [
+        '2019-11-18T21:31:17.178Z',
+        '2019-12-23T07:42:02.383Z',
+        '2020-01-28T09:15:04.904Z',
+        '2020-04-01T10:17:24.185Z',
+        '2020-05-08T14:11:59.604Z',
+        '2020-05-27T17:01:17.194Z',
+        '2024-09-10T23:36:17.929Z',
+        '2024-09-13T10:51:36.790Z',
+    ],
+    currency: 'INR',
+    locale: 'en-IN',
+}
+
+const accounts = [account1, account2];
 // ///////////////////////////////////////////////////////
 // selecting elements
 
